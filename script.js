885a9eaeb98c296b3d966e313dfce2a2c0fec231
maki <komirimaha@icloud.com>
2023-02-16 21:19:01 +0900

    HTML CSS JS基礎構文
    index.html
    script.js
    style.css

---
+++ script.js
@@ -0,0 +1,22 @@
+const pullDownButton = document.getElementById("lists")
+
+const pullDownParents = document.getElementById("pull-down")
+
+pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color:blue;")
+})
+
+pullDownButton.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color:red;")
+})
+
+pullDownButton.addEventListener('click', function(){
+    this.setAttribute("style", "background-color:green;");
+    if (pullDownParents.getAttribute("style") =="display:block;"){
+        pullDownParents.removeAttribute("style", "display:block;")
+        console.log("非表示")
+    } else {
+        pullDownParents.setAttribute("style", "display:block;")
+        console.log("表示")
+    }
+})
